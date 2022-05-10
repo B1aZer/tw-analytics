287e74659d2db2cb954477d2994c76f35d4b59aa
concave-dash <kwerkee12@gmail.com>
2022-05-10 00:26:27 -0400

    add comments, reserve api

--- get_recent_following.py
+++ get_recent_following.py
@@ -10,29 +10,36 @@ os.environ.update(dotenv)
 follow_count = 20
 
 # TODO: if you already in timeout
-#print('sleeping')
-#time.sleep(900)
+# print('sleeping')
+# time.sleep(900)
 
-consumer_key = os.getenv('consumer_key')
-consumer_secret = os.getenv('consumer_secret')
-access_token = os.getenv('access_token')
-access_token_secret = os.getenv('access_token_secret')
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(os.getenv('consumer_key'),
+                           os.getenv('consumer_secret'))
+auth.set_access_token(os.getenv('access_token'),
+                      os.getenv('access_token_secret'))
 api = tweepy.API(auth)
+
+
+auth2 = tweepy.OAuthHandler(os.getenv('api_key'), os.getenv('api_secret'))
+auth2.set_access_token(os.getenv('api_access_token'),
+                       os.getenv('api_token_secret'))
+api2 = tweepy.API(auth2)
+
 
 # read influential list
 influental_list = None
 with open('influential.txt') as f:
     influental_list = f.readlines()
 
+# write following ids, by 20
 with open('following.txt', 'w') as f:
     for influencer in influental_list:
         name = influencer.split(' ')[0]
         # find recent following
         # import pdb; pdb.set_trace()
-        following_ids = tweepy.Cursor(api.friends_ids, screen_name=name).items(follow_count)
+        # TODO: 15 req x 15 min limit
+        following_ids = tweepy.Cursor(
+            api.friends_ids, screen_name=name).items(follow_count)
         # write to file
         for f_id in following_ids:
             f.write(str(f_id) + '\r\n')
@@ -45,21 +52,24 @@ with open('following.txt') as f:
 with open('following_prev.txt') as f:
     following_ids_old = f.readlines()
 
+# TODO: 900 req x 15 min
 new_following = []
 for fol_id in following_ids_new:
     if fol_id not in following_ids_old:
         user = api.get_user(fol_id)
         new_following.append(user)
 
+# order by date created
 with open('new_following_list.txt', 'w') as f:
-    # order by date created
     for user in sorted(new_following, key=(lambda x: x.created_at), reverse=True):
         f.write(user.screen_name + " | ")
         f.write(str(user.created_at) + " | ")
         f.write(str(user.followers_count) + " | ")
         f.write("\r\n")
 
-copyfile("new_following_list.txt", "lists/following-lists/%s.txt" % (datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f%z')))
+# copy to lists
+copyfile("new_following_list.txt", "lists/following-lists/%s.txt" %
+         (datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f%z')))
 
 # copy following to prev
 copyfile("following.txt", "following_prev.txt")
