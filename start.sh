f74dbaf65849bbb9a21c9e036530358bba2db110
concave-dash <kwerkee12@gmail.com>
2022-05-10 00:34:10 -0400

    add 3 retries

--- start.sh
+++ start.sh
@@ -1,3 +1,21 @@
 cd /home/hipi/Sites/GooDee/twitter_followers
 source venv/bin/activate
 python get_recent_following.py | ts '[%Y-%m-%d %H:%M:%.S]' >> /home/hipi/Sites/GooDee/_airflow/recent_followers.log
+status=$?
+## repeat after 15m ## 
+if [ $status -ne 0 ]; then
+    sleep 15m
+    python get_recent_following.py | ts '[%Y-%m-%d %H:%M:%.S]' >> /home/hipi/Sites/GooDee/_airflow/recent_followers.log
+fi
+status=$?
+## repeat after 15m ## 
+if [ $status -ne 0 ]; then
+    sleep 15m
+    python get_recent_following.py | ts '[%Y-%m-%d %H:%M:%.S]' >> /home/hipi/Sites/GooDee/_airflow/recent_followers.log
+fi
+status=$?
+## repeat after 15m ## 
+if [ $status -ne 0 ]; then
+    sleep 15m
+    python get_recent_following.py | ts '[%Y-%m-%d %H:%M:%.S]' >> /home/hipi/Sites/GooDee/_airflow/recent_followers.log
+fi
